a4ddb68dbd9e8a09f4c4a7918ae33337ee020f9f
Brandon McGhee <bmcghee39@gmail.com>
2020-02-18 17:48:13 -0500

    new java project

---
+++ src/main/java/com/bm/jhsfootball/JhsFootballApp.java
@@ -0,0 +1,12 @@
+package com.bm.jhsfootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhsFootballApp {
+    public static void main(String[] args) {
+        SpringApplication.run(JhsFootballApp.class, args);
+
+    }
+}
